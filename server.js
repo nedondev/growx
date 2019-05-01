45500d07c8b50f1ae978c92bb6c351af9bbdab2b
nedondev <nedondev@gmail.com>
2019-05-01 19:29:51 +0700

    Add: cors expressjs

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
     port = process.env.PORT || 8000;
+var cors = require('cors')
 var sql_connect = require('./db.js');
 
+var cors = require('cors')
 app.listen(port);
 
 console.log('GrowX RESTful API started on: ' + port );
